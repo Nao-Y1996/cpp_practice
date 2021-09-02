0dfa3a49c07d52bb2e92f5b54d24d6f6a6f59a22
Nao-Y1996 <60519909+Nao-Y1996@users.noreply.github.com>
2021-09-02 17:03:44 +0900

    add array.cpp

---
+++ array.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+// void printArray(int array[]){
+//     for 
+// }
+
+int main(){
+    const int n = 5;
+    int array[n] = {1,2,3,4,5};
+    cout << "配列の最初の要素のアドレスを出力：" << &array[0] << endl;
+    cout << "配列名を出力：" << array << endl;
+    cout << "---> 配列名は最初の要素のアドレスを表している！" << endl << endl;
+
+    for (int i=0; i<n; i++){
+        cout << "array+" << i << " = " << array+i << endl;
+        cout << "*(array+" << i << ") = " << *(array+i) << endl;
+    }
+    cout << "---> 配列のある要素のアドレスに+1すると、次の要素のアドレスを表す！" << endl << endl;
+
+    int* pt = &array[0];
+    cout << "pt = &array[0]の時, pt = " << pt << endl;
+    cout << "配列名は配列の先頭要素のアドレスなので当然, array = " << array << endl;
+    for (int i=0; i<n; i++){
+        cout << "pt[" << i << "] = " << pt[i] << ",  array[" << i << "] = " << array[i] << endl;
+        // cout << "*(array+" << i << ") = " << *(array+i) << endl;
+    }
+    cout << "---> 配列名arrayはあるアドレスを表す。array[0]はそのアドレスの値を表し、array[1]はそのアドレスの隣のアドレスの値を表す。" << endl << endl;
+
+    cout << "配列の要素数：" << array+n - array << endl;
+    
+    return 0;
+}
